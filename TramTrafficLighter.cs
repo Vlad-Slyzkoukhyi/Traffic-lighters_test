fd3082a58a3cd8fff866acb0dbde9f3e1581fd85
Vladyslav_Slyzkouhyi <118803918+Vlad-Slyzkoukhyi@users.noreply.github.com>
2023-03-11 12:56:56 +0200

    Delete TramTrafficLighter.cs

--- TramTrafficLighter.cs
+++
@@ -1,7 +0,0 @@
-﻿//Class for tram traffic lighters. Inheritance from human traffic lighter.
-public class TramTrafficLight : HumanTrafficLight
-{
-    public TramTrafficLight(string name) : base(name)
-    {
-    }
-}
